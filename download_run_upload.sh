71c801a2a1a0cbcbccb41e27c6edc6569fcd1b35
ahassan@azavea.com <adeel.hassan93@gmail.com>
2019-07-05 12:32:55 -0400

    update docker script

---
+++ download_run_upload.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT_LOC=$1
+SCRIPT_NAME=$2
+OUTPUT_LOC=$4
+OUTPUT_NAME=$3
+
+aws s3 cp $SCRIPT_LOC $SCRIPT_NAME
+
+shift
+shift
+shift
+shift
+
+python ./$SCRIPT_NAME $*
+
+aws s3 cp $OUTPUT_NAME $OUTPUT_LOC
+

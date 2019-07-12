9216b114c8b3f1ceeec39584fa6664fc27485836
James W. McClain <jmcclain@azavea.com>
2019-07-12 19:59:13 -0400

    Remove `SCRIPT_NAME` Command Line Argument

--- download_run_upload.sh
+++ download_run_upload.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 SCRIPT_LOC=$1
-SCRIPT_NAME=$2
-OUTPUT_NAME=$3
-OUTPUT_LOC=$4
+SCRIPT_NAME=$(basename $SCRIPT_LOC)
+OUTPUT_NAME=$2
+OUTPUT_LOC=$3
 
 
 aws s3 cp $SCRIPT_LOC $SCRIPT_NAME
 
-shift
 shift
 shift
 shift
